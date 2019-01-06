58a3c3f71dfbb440c45e5b17315864f551a3a0fd
OgheneMichael <zonalmeet@gmail.com>
2019-01-06 02:19:35 +0100

    Refactored App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,13 @@
-var React = require("react");
-var ReactRouter = require("react-router-dom");
-var Router = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-var Switch = ReactRouter.Switch;
-var Nav = require("./Nav");
-var Home = require("./Home");
-var Battle = require("./Battle");
-var Popular = require("./Popular");
-var Results = require("./Results");
+const React = require("react");
+const ReactRouter = require("react-router-dom");
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+const Nav = require("./Nav");
+const Home = require("./Home");
+const Battle = require("./Battle");
+const Popular = require("./Popular");
+const Results = require("./Results");
 
 class App extends React.Component {
 	render() {
@@ -21,8 +21,8 @@ class App extends React.Component {
 						<Route path="/battle/results" component={Results} />
 						<Route path="/popular" component={Popular} />
 						<Route
-							render={function() {
-								return <p>Not found</p>;
+							render={() => {
+								<p>Not found</p>;
 							}}
 						/>
 					</Switch>
